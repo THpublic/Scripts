7084abdcfa420ed14cffe1a5e0481766b6c447bc
THpublic <107477839+THpublic@users.noreply.github.com>
2022-06-14 13:00:02 +0100

    Update ZDriveMap.bat

--- ZDriveMap.bat
+++ ZDriveMap.bat
@@ -1,16 +1,10 @@
-''''''''''''''''''''''Mount Z Drive to My Documents''''''''''''''
-On Error Resume Next
-
-ProfilePath = CreateObject("WScript.Shell").ExpandEnvironmentStrings("%UserProfile%\My Documents")
-
-strDirectory= """"&ProfilePath&""""
-
-Set objFSO = CreateObject("Scripting.FileSystemObject")
-Set objShell = WScript.CreateObject("WScript.Shell")
-
-If objFSO.FolderExists(ProfilePath) Then
-	objShell.run "c:\windows\system32\subst.exe Z:  "&strDirectory&"", 0, True
-Else
-	Set objFolder = objFSO.CreateFolder(ProfilePath)
-	objShell.run "c:\windows\system32\subst.exe Z:  "&strDirectory&"", 0, True
-End If
+set folder=c:\users\%username%\temp
+IF EXIST %folder% goto del
+:del
+rmdir "%folder%" /s /q
+goto next
+:next
+md %folder%
+subst z: %folder% 
+subst s: c:\start
+copy c:\windows\*.xml c:\start
