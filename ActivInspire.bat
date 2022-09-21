cc4e695fdd194f795e8b0da1e1b3a32a0efd783c
THpublic <107477839+THpublic@users.noreply.github.com>
2022-09-21 14:08:04 +0100

    Update ActivInspire.bat

--- ActivInspire.bat
+++ ActivInspire.bat
@@ -1,16 +1,16 @@
-@ECHO OFF
+REM @ECHO OFF
 REM Check if we are a 32 or 64 bit operating system.
-FOR /f %%a in ('wmic os get osarchitecture^|find /i "bit"') do set "bits=%%a"
+REM FOR /f %%a in ('wmic os get osarchitecture^|find /i "bit"') do set "bits=%%a"
 
-ECHO Downloading and installing Active Inspire
-IF NOT EXIST C:\FAR_WORK MKDIR C:\FAR_WORK
-IF NOT EXIST C:\FAR_WORK\ActivInspire.mst powershell.exe -command "& {Invoke-WebRequest -Uri "https://faronicsinternational.s3.eu-west-2.amazonaws.com/ActivInspire.mst" -OutFile "c:\FAR_WORK\ActivInspire.mst"};"
-IF NOT EXIST C:\FAR_WORK\ActivInspire.msi powershell.exe -command "& {Invoke-WebRequest -Uri "https://faronicsinternational.s3.eu-west-2.amazonaws.com/ActivInspire.msi" -OutFile "c:\FAR_WORK\ActivInspire.msi"};"
+REM ECHO Downloading and installing Active Inspire
+REM IF NOT EXIST C:\FAR_WORK MKDIR C:\FAR_WORK
+REM IF NOT EXIST C:\FAR_WORK\ActivInspire.mst powershell.exe -command "& {Invoke-WebRequest -Uri "https://faronicsinternational.s3.eu-west-2.amazonaws.com/ActivInspire.mst" -OutFile "c:\FAR_WORK\ActivInspire.mst"};"
+REM IF NOT EXIST C:\FAR_WORK\ActivInspire.msi powershell.exe -command "& {Invoke-WebRequest -Uri "https://faronicsinternational.s3.eu-west-2.amazonaws.com/ActivInspire.msi" -OutFile "c:\FAR_WORK\ActivInspire.msi"};"
 
 REM Install the downloaded file on the client system.
 
-timeout /t 60
+REM timeout /t 60
 
-msiexec.exe /i C:\FAR_WORK\ActivInspire.msi TRANSFORMS=C:\FAR_WORK\ActivInspire.mst /qn
+REM msiexec.exe /i C:\FAR_WORK\ActivInspire.msi TRANSFORMS=C:\FAR_WORK\ActivInspire.mst /qn
 
-REM msiexec.exe /i \\desktop-20pm8v8\Software\ActivInspireV2\ActivInspire.msi TRANSFORMS=\\desktop-20pm8v8\Software\ActivInspireV2\ActivInspire.mst /qn
+msiexec.exe /i \\desktop-20pm8v8\Software\ActivInspireV2\ActivInspire.msi TRANSFORMS=\\desktop-20pm8v8\Software\ActivInspireV2\ActivInspire.mst /qn
