69263aeb35ca7ea66336838048de7f1d3c07bdcd
THpublic <107477839+THpublic@users.noreply.github.com>
2022-11-28 16:22:10 +0000

    Add files via upload

---
+++ CopyNLPrintDrv.ps1
@@ -0,0 +1,17 @@
+﻿#github url to download zip file
+#Assign zip file url to local variable
+
+$Url = "https://github.com/THpublic/Scripts/raw/main/PrinterDrivers.zip"
+
+$DownloadZipFile = "C:\" + $(Split-Path -Path $Url -Leaf)
+
+$ExtractPath = "C:\"
+
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+
+$ExtractShell = New-Object -ComObject Shell.Application 
+
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
+Start-Process $ExtractPath
