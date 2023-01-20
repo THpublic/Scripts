c3737d2dc73ddc542f05de74d58788a3f75b0d57
THpublic <107477839+THpublic@users.noreply.github.com>
2023-01-20 15:55:45 +0000

    Add files via upload

---
+++ DL_All_VP_Scripts.ps1
@@ -0,0 +1,17 @@
+#github url to download zip file
+#Assign zip file url to local variable
+
+$Url = "https://github.com/THpublic/Scripts/raw/main/NL_VP_Map.zip"
+
+$DownloadZipFile = "C:\" + $(Split-Path -Path $Url -Leaf)
+
+$ExtractPath = "C:\Windows\System32"
+
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+
+$ExtractShell = New-Object -ComObject Shell.Application 
+
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
+Start-Process $ExtractPath
