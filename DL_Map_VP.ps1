88acccd56c64dc99bf852200e2964d3621f3c2e2
THpublic <107477839+THpublic@users.noreply.github.com>
2023-01-23 12:30:59 +0000

    Update DL_Map_VP.ps1

--- DL_Map_VP.ps1
+++ DL_Map_VP.ps1
@@ -5,11 +5,9 @@ $destination = 'c:\Windows\System32\RemoveAddVP_Subnet_v2.vbs'
 #Download the file
 Invoke-WebRequest -Uri $source -OutFile $destination
 
-$path = "C:\Users\Netloan_User\NTUSER.DAT"
-reg load HKU\Netloan_User $path
-
-$key = "HKU\Netloan_User\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-$name = "MapVPPrinter"
-$data = "wscript.exe `"C:\Windows\system32\invisible.vbs`" `"C:\windows\System32\RemoveAddVP_Subnet_v2.vbs`""
-
-New-ItemProperty -Path $key -Name $name -Value $data -PropertyType String -Force
+# Source file location
+$source = 'https://raw.githubusercontent.com/THpublic/Scripts/main/AddVP_Reg.bat'
+# Destination to save the file
+$destination = 'c:\Windows\System32\AddVP_Reg.bat'
+#Download the file
+Invoke-WebRequest -Uri $source -OutFile $destination
