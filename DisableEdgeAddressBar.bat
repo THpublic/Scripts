2be14614ded7b33dc0171281d54c7f607d48ed8d
THpublic <107477839+THpublic@users.noreply.github.com>
2023-03-28 14:48:20 +0100

    Update DisableEdgeAddressBar.bat

--- DisableEdgeAddressBar.bat
+++ DisableEdgeAddressBar.bat
@@ -1,1 +1,1 @@
-reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge" /v AddressBarDisabled /t REG_DWORD /d 1 /f
+reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge" /v KioskAddressBarEditingEnabled /t REG_DWORD /d 0 /f
