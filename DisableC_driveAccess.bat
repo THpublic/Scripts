fd28ac382f275f4224e9987d1a77adcb473b442a
THpublic <107477839+THpublic@users.noreply.github.com>
2023-03-30 13:45:35 +0100

    Update and rename DisableC_driveAccess.bat to DisableC_driveAccessAllUsers.bat

--- DisableC_driveAccess.bat
+++
@@ -1,3 +0,0 @@
-reg load HKU\ISLab_User C:\Users\ISLab_User\NTUSER.DAT
-
-reg add "HKEY_Users\ISLab_User\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoViewOnDrive" /t REG_DWORD /d 4 /f
