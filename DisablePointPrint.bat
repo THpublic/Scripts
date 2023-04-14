92c373839452d3e2c36fd0458ba110ab334cb790
THpublic <107477839+THpublic@users.noreply.github.com>
2023-04-14 16:40:54 +0100

    Update DisablePointPrint.bat

--- DisablePointPrint.bat
+++ DisablePointPrint.bat
@@ -1,2 +1,4 @@
-﻿Reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointandPrint" /v "RestrictDriverInstallationToAdministrators" /t REG_DWORD /d "0" /f
+﻿REM Reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointandPrint" /v "RestrictDriverInstallationToAdministrators" /t REG_DWORD /d "0" /f
+ 
+ reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v RestrictDriverInstallationToAdministrators /t REG_DWORD /d 0 /f
 
