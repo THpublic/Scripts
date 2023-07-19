77b823123291b927ae689ffd9f5d843814478d49
THpublic <107477839+THpublic@users.noreply.github.com>
2023-07-19 11:41:23 +0100

    Update DisableIPv6.bat

--- DisableIPv6.bat
+++ DisableIPv6.bat
@@ -1,4 +1,5 @@
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /t REG_DWORD /d 255 /f
+netsh interface ip set dnsservers name="Ethernet" source=dhcp
 @echo off
 arp -d *
 ipconfig /release
