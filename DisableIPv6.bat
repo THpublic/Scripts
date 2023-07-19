eca4ec8f014d516b1f63259536430ac2c2fdc195
THpublic <107477839+THpublic@users.noreply.github.com>
2023-07-19 11:17:22 +0100

    Update DisableIPv6.bat

--- DisableIPv6.bat
+++ DisableIPv6.bat
@@ -1,2 +1,8 @@
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /t REG_DWORD /d 255 /f
-ipconfig /release && ipconfig /renew
+@echo off
+arp -d *
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+ipconfig /registerdns
+echo.
