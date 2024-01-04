dc16901c65d3541f93e49fb85ec4903f075f343b
THpublic <107477839+THpublic@users.noreply.github.com>
2024-01-04 14:58:55 +0000

    Update DL_OfficeUninstaller.ps1

--- DL_OfficeUninstaller.ps1
+++ DL_OfficeUninstaller.ps1
@@ -18,5 +18,5 @@ $ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items()
 $ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
 Start-Process $ExtractPath
 
-Cscript.exe "C:\windows\system32\OFFICEREMOVA-offscrubl\o365\OffScrubc2r.vbs” ALL /Quiet /NoCancel /Force /OSE
+Cscript.exe "C:\windows\system32\OFFICEREMOVA-offscrubl\o365\OffScrubc2r.vbs" ALL /Quiet /NoCancel /Force /OSE
 
