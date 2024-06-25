d79f54546e945b2f2d28e48ffbdae409cdc59320
THpublic <107477839+THpublic@users.noreply.github.com>
2024-06-25 11:41:34 +0100

    Update and rename DeleteBrowserHistory.bat to BrowserSettings.bat

--- BrowserSettings.bat
+++ BrowserSettings.bat
@@ -1,1 +1,2 @@
 Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "ClearBrowsingDataOnExit" /t REG_DWORD /d "1" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "PasswordManagerEnabled" /t REG_DWORD /d "0" /f
