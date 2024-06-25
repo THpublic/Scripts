2af5cdf960a372ce605149fd23c611fcd9a5b8b1
THpublic <107477839+THpublic@users.noreply.github.com>
2024-06-25 11:57:12 +0100

    Update BrowserSettings.bat

--- BrowserSettings.bat
+++ BrowserSettings.bat
@@ -1,3 +1,4 @@
 Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "ClearBrowsingDataOnExit" /t REG_DWORD /d "1" /f
 Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "PasswordManagerEnabled" /t REG_DWORD /d "0" /f
 Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "WebWidgetIsEnabledOnStartup" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Edge" /v "HideFirstRunExperience" /t REG_DWORD /d "1" /f
