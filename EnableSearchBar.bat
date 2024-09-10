daa303b0f7f244099bb506ee7427014ead3f3ba5
THpublic <107477839+THpublic@users.noreply.github.com>
2024-09-10 17:48:22 +0100

    Update EnableSearchBar.bat

--- EnableSearchBar.bat
+++ EnableSearchBar.bat
@@ -1,2 +1,3 @@
-Reg.exe add "HKLM\SOFTWARE\Microsoft\PolicyManager\default\Search\DisableSearch" /v "value" /t REG_DWORD /d "1" /f
-Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "DisableSearch" /t REG_DWORD /d "1" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\PolicyManager\default\Search\DisableSearch" /v "value" /t REG_DWORD /d "0" /f
+Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "DisableSearch" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /f
