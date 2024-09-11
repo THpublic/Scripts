845a547dce793a92f61df0d522bb299defeba77e
THpublic <107477839+THpublic@users.noreply.github.com>
2024-09-11 12:40:30 +0100

    Update EnableHighPerPowerMode.bat

--- EnableHighPerPowerMode.bat
+++ EnableHighPerPowerMode.bat
@@ -1,4 +1,4 @@
-powercfg.exe /setactive e9a42b02-d5df-448d-aa00-03f14749eb61
+powercfg /setactive e9a42b02-d5df-448d-aa00-03f14749eb61
 
 powercfg /SETACVALUEINDEX SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
 
