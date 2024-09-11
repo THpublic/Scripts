d64e5be28aa391f8a86eb0068f542b4ab070584c
THpublic <107477839+THpublic@users.noreply.github.com>
2024-09-11 12:19:00 +0100

    Update EnableHighPerPowerMode.ps1

--- EnableHighPerPowerMode.ps1
+++ EnableHighPerPowerMode.ps1
@@ -1,5 +1,5 @@
 # Set "High Performance" power plan
-powercfg.exe /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+powercfg.exe /setactive e9a42b02-d5df-448d-aa00-03f14749eb61
 
 # Disable sleep for the PC
 powercfg.exe /change monitor-timeout 0
