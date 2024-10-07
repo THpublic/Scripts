711faa65126dadb8819a79b76c51c70435b94f62
THpublic <107477839+THpublic@users.noreply.github.com>
2024-10-07 17:36:26 +0100

    Create PrinchVP_ISB.ps1

---
+++ PrinchVP_ISB.ps1
@@ -0,0 +1,22 @@
+# Hardcoded values
+$Id = "110564"
+$Name = "Idea Store Bow Public Printer"
+
+New-CimInstance -Namespace root/DEFAULT -ClassName Princh_PrinterPort -Property @{
+	PortName = $Id
+	FilePattern = "%U\AppData\Local\Princh\PCP\Temp\$Id\%j"
+	UserCommand = "`"C:\Program Files\Princh\PrinchCloudPrinter\node.cmd`" client.js $Id `"%f`" `"%u`" `"%U`" `"%t`" `"%j`""
+	ExecPath = "C:\Program Files\Princh\PrinchCloudPrinter"
+}
+
+Restart-Service -Force Spooler
+
+$RegionName = [System.Globalization.RegionInfo]::CurrentRegion.ThreeLetterISORegionName
+$NaRegions = @("USA", "CAN", "MEX")
+$DriverSuffix = If ($NaRegions.Contains($RegionName)) {
+	"(NA)"
+} Else {
+	"(ISO)"
+}
+
+Add-Printer -Name $Name -PortName $Id -DriverName "Princh Cloud Printer $DriverSuffix" -PrintProcessor "winprint"
