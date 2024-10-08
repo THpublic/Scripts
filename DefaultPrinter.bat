e896e0c4672bf466cedebcdf09bc8a9567e5230c
THpublic <107477839+THpublic@users.noreply.github.com>
2024-10-08 15:58:56 +0100

    Update DefaultPrinter.bat

--- DefaultPrinter.bat
+++ DefaultPrinter.bat
@@ -1,6 +1,6 @@
 reg load HKU\Netloan_User C:\Users\Netloan_User\NTUSER.DAT
 
-reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v "Device" /t REG_SZ /d "Idea Store Bow Public Printer,winspool,Ne00:" /f
-reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v "IsMRUEstablished" /t REG_DWORD /d "0" /f
-reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v "LegacyDefaultPrinterMode" /t REG_DWORD /d "1" /f
-reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v "MenuDropAlignment" /t REG_SZ /d "0" /f
+reg add "HKEY_Users\Netloan_User\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v "Device" /t REG_SZ /d "Idea Store Bow Public Printer,winspool,Ne00:" /f
+reg add "HKEY_Users\Netloan_User\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v "IsMRUEstablished" /t REG_DWORD /d "0" /f
+reg add "HKEY_Users\Netloan_User\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v "LegacyDefaultPrinterMode" /t REG_DWORD /d "1" /f
+reg add "HKEY_Users\Netloan_User\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v "MenuDropAlignment" /t REG_SZ /d "0" /f
