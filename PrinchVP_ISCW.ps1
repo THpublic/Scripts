01636f95733e20f1c278895099ee2e7b5a85bd0e
THpublic <107477839+THpublic@users.noreply.github.com>
2024-10-17 11:46:12 +0100

    Create PrinchVP_ISCW.ps1

---
+++ PrinchVP_ISCW.ps1
@@ -0,0 +1,32 @@
+Get-Printer | Where-Object { $_.Name -notmatch "PDF|txt|fax|usb|enhanced|epson|microsoft" } | Remove-Printer
+# Hardcoded values
+$Id = "110565"
+$Name = "Idea Store Canary Wharf Public Printer"
+
+New-CimInstance -Namespace root/DEFAULT -ClassName Princh_PrinterPort -Property @{
+	PortName = $Id
+	FilePattern = "%U\AppData\Local\Princh\PCP\Temp\$Id\%j"
+	UserCommand = "`"C:\Program Files\Princh\PrinchCloudPrinter\node.cmd`" client.js $Id `"%f`" `"%u`" `"%U`" `"%t`" `"%j`""
+	ExecPath = "C:\Program Files\Princh\PrinchCloudPrinter"
+}
+
+Restart-Service -Force Spooler
+
+$RegionName = [System.Globalization.RegionInfo]::CurrentRegion.ThreeLetterISORegionName
+$NaRegions = @("USA", "CAN", "MEX")
+$DriverSuffix = If ($NaRegions.Contains($RegionName)) {
+	"(NA)"
+} Else {
+	"(ISO)"
+}
+
+Add-Printer -Name $Name -PortName $Id -DriverName "Princh Cloud Printer $DriverSuffix" -PrintProcessor "winprint"
+
+# Reg2CI (c) 2022 by Roger Zander
+reg.exe load HKU\Netloan_User C:\Users\Netloan_User\NTUSER.DAT
+
+if((Test-Path -LiteralPath 'HKEY_Users:\Netloan_User\Software\Microsoft\Windows NT\CurrentVersion\Windows') -ne $true) {  New-Item "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows" -force -ea SilentlyContinue };
+New-ItemProperty -LiteralPath 'HKEY_Users:\Netloan_User\Software\Microsoft\Windows NT\CurrentVersion\Windows' -Name 'Device' -Value 'Idea Store Canary Wharf Public Printer,winspool,Ne00:' -PropertyType String -Force -ea SilentlyContinue;
+New-ItemProperty -LiteralPath 'HKEY_Users:\Netloan_User\Software\Microsoft\Windows NT\CurrentVersion\Windows' -Name 'IsMRUEstablished' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
+New-ItemProperty -LiteralPath 'HKEY_Users:\Netloan_User\Software\Microsoft\Windows NT\CurrentVersion\Windows' -Name 'LegacyDefaultPrinterMode' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
+New-ItemProperty -LiteralPath 'HKEY_Users:\Netloan_User\Software\Microsoft\Windows NT\CurrentVersion\Windows' -Name 'MenuDropAlignment' -Value '0' -PropertyType String -Force -ea SilentlyContinue;
