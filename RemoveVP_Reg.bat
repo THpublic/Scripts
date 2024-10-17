6f110ccfabd3cbf2a296fa5c7f1e5252ee2867a8
THpublic <107477839+THpublic@users.noreply.github.com>
2024-10-17 13:16:42 +0100

    Update RemoveVP_Reg.bat

--- RemoveVP_Reg.bat
+++ RemoveVP_Reg.bat
@@ -1,3 +1,3 @@
 reg load HKU\Netloan_User C:\Users\Netloan_User\NTUSER.DAT
 
-reg delete "HKU\Netloan_User\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "MapVPPrinter" /t REG_SZ /d "wscript.exe \"C:\Windows\system32\invisible.vbs\" \"C:\windows\System32\RemoveAddVP_Subnet_v2.vbs\"" /f
+reg delete "HKU\Netloan_User\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "MapVPPrinter" /f
