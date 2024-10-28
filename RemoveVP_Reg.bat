b2e1c88e5551787099816b6967a1ff5809d45d65
THpublic <107477839+THpublic@users.noreply.github.com>
2024-10-28 16:48:11 +0000

    Update RemoveVP_Reg.bat

--- RemoveVP_Reg.bat
+++ RemoveVP_Reg.bat
@@ -1,3 +1,4 @@
 reg load HKU\Netloan_User C:\Users\Netloan_User\NTUSER.DAT
 
 reg delete "HKU\Netloan_User\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "MapVPPrinter" /f
+reg delete "HKU\Netloan_user\Printers\Connections" /f
