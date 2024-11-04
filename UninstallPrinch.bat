c079248bcdaba406b256d84be578ab8ccfc3f71d
THpublic <107477839+THpublic@users.noreply.github.com>
2024-11-04 16:33:10 +0000

    Update and rename UninstallPrinch.ps1 to UninstallPrinch.bat

---
+++ UninstallPrinch.bat
@@ -0,0 +1,2 @@
+msiexec.exe /x{6E0633EB-9246-4E95-8E9E-35E3046B913B} /qn
+
--- UninstallPrinch.ps1
+++
@@ -1,8 +0,0 @@
-
-Set-ExecutionPolicy RemoteSigned
-# Define the product code of the MSI to uninstall
-# You need to specify the ProductCode of the MSI package you want to uninstall
-$productCode = "{6E0633EB-9246-4E95-8E9E-35E3046B913B}"
-
-# Uninstall the MSI file silently
-Start-Process "msiexec.exe" -ArgumentList "/x $productCode /quiet /norestart" -NoNewWindow -Wait
