b670c23fe09702f3c708a3507b1bcea37ba0d31b
THpublic <107477839+THpublic@users.noreply.github.com>
2024-11-04 12:16:00 +0000

    Update DisableMonitorAudio.ps1

--- DisableMonitorAudio.ps1
+++ DisableMonitorAudio.ps1
@@ -1,1 +1,1 @@
-Disable-PnpDevice -InstanceId "Disable-PnpDevice -InstanceId "HDAUDIO\SGPC_FUNC_01&VEN_8086&DEV_2815&SUBSYS_80860101&REV_1000\4&F2E9A4&0&0201" -Confirm:$false" -Confirm:$false
+Disable-PnpDevice -InstanceId "HDAUDIO\SGPC_FUNC_01&VEN_8086&DEV_2815&SUBSYS_80860101&REV_1000\4&F2E9A4&0&0201" -Confirm:$false
