3bad1f64877d9026061dacedc72475cd7c852853
THpublic <107477839+THpublic@users.noreply.github.com>
2024-11-04 10:25:55 +0000

    Create EnableRightClickSysTray.bat

---
+++ EnableRightClickSysTray.bat
@@ -0,0 +1,2 @@
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoTrayContextMenu" /t REG_DWORD /d "0" /f
+REM Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoViewContextMenu" /t REG_DWORD /d "1" /f
