5b5ecfdcf174010f7d3fd92e7fb8d23a96298e68
THpublic <107477839+THpublic@users.noreply.github.com>
2024-11-04 10:57:54 +0000

    Update UninstallPrinch.ps1

--- UninstallPrinch.ps1
+++ UninstallPrinch.ps1
@@ -1,3 +1,5 @@
+
+Set-ExecutionPolicy RemoteSigned
 # Define the product code of the MSI to uninstall
 # You need to specify the ProductCode of the MSI package you want to uninstall
 $productCode = "{6E0633EB-9246-4E95-8E9E-35E3046B913B}"
