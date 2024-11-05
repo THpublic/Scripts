9533106f51701a1afe9e73c9a08c7c1179e671a5
THpublic <107477839+THpublic@users.noreply.github.com>
2024-11-05 16:10:12 +0000

    Create DisableAudioJackDetect.bat

---
+++ DisableAudioJackDetect.bat
@@ -0,0 +1,5 @@
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}\0001\GlobalSettings" /v "EnableDynamicDevices" /t REG_BINARY /d "00000000" /f
+
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}\0000\GlobalSettings" /v "EnableDynamicDevices" /t REG_BINARY /d "00000000" /f
+
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e96c-e325-11ce-bfc1-08002be10318}\0001\Settings" /v "JackCtrl" /t REG_BINARY /d "ff824000" /f
