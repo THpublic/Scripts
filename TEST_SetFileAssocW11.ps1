64ccd342e229519e103ae7b101cd3b4f00d89830
THpublic <107477839+THpublic@users.noreply.github.com>
2024-11-27 12:55:06 +0000

    Update TEST_SetFileAssocW11.ps1

--- TEST_SetFileAssocW11.ps1
+++ TEST_SetFileAssocW11.ps1
@@ -4,6 +4,7 @@ reg load HKU\Netloan_User C:\Users\Netloan_User\NTUSER.DAT
 
 if((Test-Path -LiteralPath "Registry::\HKEY_USERS\Netloan_User\Software\Microsoft\Windows\CurrentVersion\Run") -ne $true) {  New-Item "Registry::\HKEY_USERS\Netloan_User\Software\Microsoft\Windows\CurrentVersion\Run" -force -ea SilentlyContinue };
 New-ItemProperty -LiteralPath 'Registry::\HKEY_USERS\Netloan_User\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'SetFileAssociationW11' -Value '"C:\Windows\system32\SetFileAssocW11.bat"' -PropertyType String -Force -ea SilentlyContinue;
+New-ItemProperty -LiteralPath 'Registry::\HKEY_USERS\Netloan_User\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name 'RunSetUserFTA' -Value '"C:\Windows\system32\SetFileAssocW11.bat"' -PropertyType String -Force -ea SilentlyContinue;
 
 # Define the URL of the MSI file and the destination path
 $url = "https://raw.githubusercontent.com/THpublic/Scripts/refs/heads/main/SetFileAssocW11.bat"
